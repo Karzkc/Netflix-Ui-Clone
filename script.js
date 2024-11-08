73c1167d44d46ccde049816b5d0081b6353961b4
Kartik Khiriya <kartikkhiriya3@gmail.com>
2024-11-08 14:08:28 +0530

    final commit

--- script.js
+++ script.js
@@ -26,24 +26,33 @@ rightbtn.addEventListener("click", () => {
     rightbtn.style.visibility = "hidden"
     leftbtn.style.visibility = "visible"
     rightbtn.style.transition = "400ms ease"
-    
+
 });
 
-document.querySelector(".exp1").addEventListener("click", () => {
-    
-
-    if (document.querySelector(".expand").style.display == "none") {
-        document.querySelector(".expand").style.display = "block";
-        document.querySelector(".faq").style.height = "962px"
-        document.querySelector(".plus-svg").style.transform = "rotate(135deg)"
-        document.querySelector(".plus-svg").style.transition = "400ms ease"
-        
-    } else if (document.querySelector(".expand").style.display = "block") {
-        document.querySelector(".faq").style.height = "690px"
-        document.querySelector(".expand").style.display = "none"; 
-        document.querySelector(".plus-svg").style.transform = "rotate(-90deg)"
-        document.querySelector(".plus-svg").style.transition = "400ms ease"
-        
+function toggleFAQ(expandclass, svg) {
+    const expandElement = document.querySelector(`.${expandclass}`);
+    const plusSvgElement = document.querySelector(`.${svg}`);
+    const faqElement = document.querySelector(".faq");
+    let currentHeight = parseInt(window.getComputedStyle(faqElement).height);
+    let height_plus = currentHeight + 250
+    let height_minus = currentHeight - 250
+    if (expandElement.style.display === "none" || expandElement.style.display === "") {
+        expandElement.style.display = "block";
+        faqElement.style.height = `${height_plus}px`;
+        plusSvgElement.style.transform = "rotate(135deg)";
+    } else {
+        expandElement.style.display = "none";
+        faqElement.style.height = `${height_minus}px`;
+        plusSvgElement.style.transform = "rotate(-90deg)";
     }
 
-});
+    plusSvgElement.style.transition = "400ms ease";
+}
+
+
+document.querySelector(".exp1").addEventListener("click", () => toggleFAQ("expand", "plus-svg"));
+document.querySelector(".exp2").addEventListener("click", () => toggleFAQ("expand2", "plus-svg2"));
+document.querySelector(".exp3").addEventListener("click", () => toggleFAQ("expand3", "plus-svg3"));
+document.querySelector(".exp4").addEventListener("click", () => toggleFAQ("expand4", "plus-svg4"));
+document.querySelector(".exp5").addEventListener("click", () => toggleFAQ("expand5", "plus-svg5"));
+document.querySelector(".exp6").addEventListener("click", () => toggleFAQ("expand6", "plus-svg6"));
